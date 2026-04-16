d1d0a389a1c3b21ea2104576dbb3724e897df094
cambriajm <cambriajm@CAMDELL>
2026-04-16 09:11:14 -0600

    Add project files.

---
+++ RentalCar/RentalCar.cs
@@ -0,0 +1,15 @@
+namespace RentalCar
+{
+    public partial class RentalCar : Form
+    {
+        public RentalCar()
+        {
+            InitializeComponent();
+        }
+
+        private void RentalCar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
