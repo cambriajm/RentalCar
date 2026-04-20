18564e2187d4a3ccdd9ed25e477cc5f816f6247d
cambriajm <cambriajm@CAMDELL>
2026-04-19 19:44:17 -0600

    aAAAAAAAAAAAAAAAA

--- RentalCar/RentalCar.cs
+++ RentalCar/RentalCar.cs
@@ -55,11 +55,229 @@ namespace RentalCar
 
         }
 
-        private bool ValidateFields()
+        private bool Message()
         {
-
+            bool valid = true;
+            string message = "";
+            if (CustomerNameTextBox.Text == "")
+            {
+                message += "Please enter your name.\n";
+            }
+            if (AddressTextBox.Text == "")
+            {
+                message += "Please enter your address.\n";
+            }
+            if (CityTextBox.Text == "")
+            {
+                message += "Please enter your city.\n";
+            }
+            if (StateTextBox.Text == "")
+            {
+                message += "Please enter your state.\n";
+            }
+            if (ZipCodeTextBox.Text == "")
+            {
+                message += "Please enter your zip code.\n";
+            }
+            try
+            {
+                if (int.Parse(BOdometerTextBox.Text) < 0 || int.Parse(BOdometerTextBox.Text) > (int.Parse(EOdometerReadTextBox.Text)))
+                {
+                    message += "Please enter Odometer reading.\n";
+                }
+            }
+            catch (Exception)
+            {
+                message += "Please enter Odometer reading.\n";
+            }
+            try
+            {
+                if ((int.Parse(EOdometerReadTextBox.Text)<0))
+                {
+                    message += "Please enter end Odometer reading.\n";
+                }
+            }
+            catch (Exception)
+            {
+                message += "Please enter end Odometer reading.\n";
+            }
+            try
+            {
+                if (int.Parse(DaysTextBox.Text)<0||int.Parse(DaysTextBox.Text)>45)
+                {
+                    message += "Please enter the number of days (0-45).\n";
+                }
+            }
+            catch (Exception)
+            {
+                message += "Please enter the number of days (0-45).\n";
+            }
+            if (message != "")
+            {
+                MessageBox.Show(message);
+            }
+            return valid;
         }
 
+        private bool ValidateFields()
+        {
+            bool valid = true;
+            if (CustomerNameTextBox.Text != "") 
+            {
+                CustomerNameTextBox.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                CustomerNameTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+            if (AddressTextBox.Text != "")
+            {
+                AddressTextBox.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                AddressTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+            if (CityTextBox.Text != "")
+            {
+                CityTextBox.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                CityTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+            if (StateTextBox.Text != "")
+            {
+                StateTextBox.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                StateTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+            if (ZipCodeTextBox.Text != "")
+            {
+                ZipCodeTextBox.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                ZipCodeTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+            try
+            {
+                if (int.Parse(BOdometerTextBox.Text) >= 0 && int.Parse(BOdometerTextBox.Text) < (int.Parse(EOdometerReadTextBox.Text)))
+                {
+                    BOdometerTextBox.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    BOdometerTextBox.BackColor = Color.LightYellow;
+                    valid = false;
+                }
+            }
+            catch (Exception)
+            {
+                BOdometerTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+            try
+            {
+                if (int.Parse(EOdometerReadTextBox.Text) >= 0)
+                {
+                    EOdometerReadTextBox.BackColor = Color.LightGreen;
+                }
+         
+                else
+                {
+                    EOdometerReadTextBox.BackColor = Color.LightYellow;
+                    valid = false;
+                }
+            }
+            catch (Exception)
+            {
+                EOdometerReadTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+
+            try
+            {
+                if  (int.Parse(DaysTextBox.Text)>=1&&int.Parse(DaysTextBox.Text)<=45)
+                {
+                    DaysTextBox.BackColor = Color.LightGreen;
+                }
+                else
+                {
+                    DaysTextBox.BackColor = Color.LightYellow;
+                    valid = false;
+                }
+            }
+            catch (Exception)
+            {
+                DaysTextBox.BackColor = Color.LightYellow;
+                valid = false;
+            }
+            return valid;
+        }
+
+
+        decimal CalcAAA(decimal anAmount)//calc AAA discount
+        {
+            decimal discount = 0;
+            if (AAADiscountCheckBox.Checked)
+            {
+                discount = anAmount * 0.5m;
+            }
+            return discount;
+        }
+
+        decimal CalcSenior(decimal anAmount)//calc senior discount
+        {
+            decimal discount = 0;
+            if (SeniorDiscountCheckBox.Checked)
+            {
+                discount = anAmount * 0.3m;
+            }
+            return discount;
+        }
+
+        private decimal CalcDrivenMiles(decimal Miles)//miles calculated and kilometer conversion
+        {
+            if (KilometersRadioButton.Checked)
+            {
+                Miles = (decimal.Parse(EOdometerReadTextBox.Text) - decimal.Parse(BOdometerTextBox.Text)) * 0.61m;
+            }
+            else if (MilesRadioButton.Checked)
+            {
+                Miles = Miles = (decimal.Parse(EOdometerReadTextBox.Text) - decimal.Parse(BOdometerTextBox.Text));
+            }
+            DistanceTextBox.Text = Miles.ToString() + "miles";
+            Miles = Miles;
+            return Miles;
+        }
+
+        private decimal CalcMilageCharge(decimal milageCharge)
+        {
+            if (miles <= 200)
+            {
+                milageCharge = 0.00m;
+                MilageChargeTextBox.Text = $"{milageCharge:C}";
+            }
+            if(miles >= 201 && miles >= 500)
+            {
+                milageCharge = miles * 0.12m;
+                MilageChargeTextBox.Text=$"{milageCharge:C}";
+            }
+            if(miles >= 501)
+            {
+                milageCharge = miles * 0.1m;
+                MilageChargeTextBox.Text=$"{milageCharge:C}"
+            }
+            return milageCharge;
+        }
          
 
 
